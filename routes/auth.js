75bf003f5945517c58d954f96999b4f1a87b4893
vivekanandhansj <vivekmech249@gmail.com>
2022-08-20 03:25:26 +0100

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+import  express from "express";
+import { signin, signup } from "../controllers/auth.js";
+
+
+const router = express.Router();
+
+// CREATE A USER
+router.post("/signup",signup)
+
+// SIGN IN
+router.post("/signin",signin)
+
+
+// GOOGLE AUTHENTICATION
+router.post("/google",)
+
+export default router;
